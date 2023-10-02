8ff8a570fac21ef47d1144a52bbabbb4f19b7acd
Sebastianriossalazar10 <144742464+Sebastianriossalazar10@users.noreply.github.com>
2023-10-02 15:59:41 -0600

    Subir archivos

---
+++ Persona.java
@@ -0,0 +1,57 @@
+package arraylistpersonas;
+
+public class Persona {
+    private int num;
+    private String nombre;
+    private int edad;
+    
+    public Persona(){    
+    }
+    public Persona(int num, String nombre, int edad){
+        this.num = num;
+        this.nombre = nombre;
+        this.edad = edad;    
+    }
+
+    /**
+     * @return the num
+     */
+    public int getNum() {
+        return num;
+    }
+
+    /**
+     * @param num the num to set
+     */
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the edad
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
